283b4e5715cb7b7d2294ee217691c51a0016e657
CantCode023 <cantcode023@gmail.com>
2023-04-06 16:46:27 +0800

    added UserAlreadyExist exception

--- db/exceptions.py
+++ db/exceptions.py
@@ -1,5 +1,6 @@
 class UserNotFound(Exception):
     """The user you're trying to find does not exist."""
 
+
 class UserAlreadyExist(Exception):
     """The user you're trying to create already exist."""
